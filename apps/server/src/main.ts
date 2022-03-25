17683d96b0152fe6112ca32f2a5dfa453441257c
uptotec <mahmoudashraf11179@gmail.com>
2022-03-25 15:27:14 +0200

    swagger setup

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,12 +2,21 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SanitizeMongooseModelInterceptor } from 'nestjs-mongoose-exclude';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new SanitizeMongooseModelInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('dating backend server')
+    .setDescription('dating API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
